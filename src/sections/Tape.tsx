4b874eb1f18d0b3f71d0c00a852c717f533c6d1b
chonny <hitsugiya@gmail.com>
2025-04-15 14:46:57 -0400

    style tape section

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -1,3 +1,35 @@
+import StarIcon from '@/assets/icons/star.svg';
+
+const words = [
+  'Performant',
+  'Responsive',
+  'Scalable',
+  'Secure',
+  'Interactive',
+  'User-friendly',
+  'Search Optimized',
+  'Reliable',
+];
+
 export const TapeSection = () => {
-  return <div>Tape Section</div>;
+  return (
+    <div>
+      <div className="py-16 lg:py-24 overflow-x-clip">
+        <div className="bg-gradient-to-r from-emerald-300 to-sky-400 -rotate-3 -mx-1">
+          <div className="flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
+            <div className="flex flex-none gap-4 py-3">
+              {words.map((word) => (
+                <div className="inline-flex items-center gap-4" key={word}>
+                  <span className="text-gray-900 uppercase text-sm font-bold">
+                    {word}
+                  </span>
+                  <StarIcon className="text-gray-900 size-6 -rotate-12" />
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
