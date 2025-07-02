5119050a89426e62546f508462dd247b7576b4ed
chonny <hitsugiya@gmail.com>
2025-07-02 17:25:03 -0400

    Animate hero and tape sections

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -1,32 +1,38 @@
-import StarIcon from '@/assets/icons/star.svg';
+import StarIcon from "@/assets/icons/star.svg";
+import { Fragment } from "react";
 
 const words = [
-  'Performant',
-  'Responsive',
-  'Scalable',
-  'Secure',
-  'Interactive',
-  'User-friendly',
-  'Search Optimized',
-  'Reliable',
+  "Performant",
+  "Responsive",
+  "Scalable",
+  "Secure",
+  "Interactive",
+  "User-friendly",
+  "Search Optimized",
+  "Reliable",
 ];
 
 export const TapeSection = () => {
   return (
-    <div>
-      <div className="py-16 lg:py-24 overflow-x-clip">
-        <div className="bg-gradient-to-r from-emerald-300 to-sky-400 -rotate-3 -mx-1">
-          <div className="flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
-            <div className="flex flex-none gap-4 py-3">
-              {words.map((word) => (
-                <div className="inline-flex items-center gap-4" key={word}>
-                  <span className="text-gray-900 uppercase text-sm font-bold">
-                    {word}
-                  </span>
-                  <StarIcon className="text-gray-900 size-6 -rotate-12" />
-                </div>
-              ))}
-            </div>
+    <div className="py-16 lg:py-24 overflow-x-clip">
+      <div
+        className="bg-gradient-to-r from-emerald-300 to-sky-400 -rotate-3 -mx-1"
+        style={{ clipPath: "polygon(0 10%, 100% 0, 100% 90%, 0 100%)" }}
+      >
+        <div className="flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
+          <div className="flex flex-none gap-4 pr-4 py-3 animate-move-left [animation-duration:30s]">
+            {[...new Array(2)].fill(0).map((_, index) => (
+              <Fragment key={index}>
+                {words.map((word) => (
+                  <div className="inline-flex items-center gap-4" key={word}>
+                    <span className="text-gray-900 uppercase text-sm font-bold">
+                      {word}
+                    </span>
+                    <StarIcon className="text-gray-900 size-6 -rotate-12" />
+                  </div>
+                ))}
+              </Fragment>
+            ))}
           </div>
         </div>
       </div>
